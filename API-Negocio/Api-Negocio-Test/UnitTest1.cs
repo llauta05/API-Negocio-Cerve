fe1d89d6ea5961604aeace856a0d058c3ad22536
Lautaro Landin <landin.lautaro@gmail.com>
2019-05-29 14:40:26 -0300

    release: add UnitTest

---
+++ API-Negocio/Api-Negocio-Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Api_Negocio_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
